2ca44c774f156cbf3d6bdf2ec2d523dc5858f0e0
Maria W <m.e.k.westlund@gmail.com>
2019-01-18 00:59:14 +0100

    h

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
   */
  app.all('/*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  //res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, application/json");
   res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
   next();
